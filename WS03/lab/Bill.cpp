6117812d3f1b0a52615f2cf849cf0c3bdb747aec
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2021-01-31 20:00:53 -0500

    Workshop 3

---
+++ WS03/lab/Bill.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "cstring.h"
+using namespace std;
+#include "Bill.h"
+namespace sdds {
+
+
+}
---
+++ WS03/lab/Item.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "cstring.h"
+using namespace std;
+#include "Item.h"
+namespace sdds {
+
+
+}
