f614ce0c64e38433c2ccc911bc1eed28e2fa6624
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2021-02-01 08:24:12 -0500

    reordered includes

--- WS03/lab/Bill.cpp
+++ WS03/lab/Bill.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "cstring.h"
-using namespace std;
 #include "Bill.h"
+using namespace std;
 namespace sdds {
 
 
--- WS03/lab/Item.cpp
+++ WS03/lab/Item.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "cstring.h"
-using namespace std;
 #include "Item.h"
+using namespace std;
 namespace sdds {
 
 
