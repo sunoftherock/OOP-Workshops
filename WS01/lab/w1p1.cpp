94c389782f282a3f5efdfafee2c75533f9aa6b7a
Rocky <me.rocky.sun@gmail.com>
2021-02-03 14:29:01 -0500

    test

--- WS01/lab/w1p1.cpp
+++ WS01/lab/w1p1.cpp
@@ -1,13 +1,4 @@
-// Workshop 1:
-// Version: 1.0
-//   fixed bug for deleting the first item. 
-//   this fix did not effect submission results
-// Date: 2021/10/16
-// Author: Fardad Soleimanloo
-// Description:
-// w1p1.cpp
-//
-///////////////////////////////////////////////////
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <iostream>
